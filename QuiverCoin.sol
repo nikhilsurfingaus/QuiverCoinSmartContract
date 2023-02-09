656e0fcbe570b67c9ebba91432a4fc83e0761959
Nikhil Naik <46309727+nikhilsurfingaus@users.noreply.github.com>
2023-02-09 19:32:14 +1100

    Update QuiverCoin.sol

--- QuiverCoin.sol
+++ QuiverCoin.sol
@@ -1,66 +1,105 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.5.0;
 
-//The following smart contarct coincides with the BEP-20 Binance BNB Smart Chain (BSC)Token Standard
-contract QuiverCoin {
+//---------------------------------------------------------
+//           ERC Token Standard #20 Interface
+//---------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
-    //API Grabbers Constant Values
-    uint public totalSupply = 1000000 * 10 ** 18;
-    string public name = "QuiverCoin";
-    string public symbol = "QIVC";
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
 
-    //decimals refers to how divisible a token can be, from 0 (not at all divisible) 
-    //to 18 (pretty much continuous) and even higher if required. 
-    uint public decimals = 18;
-
-    //Balances Mapping Record
-    mapping(address => uint) public balances; 
-    
-    //Outer address is the owner of the token, each token owner have series of nested mapping 
-    //with address of sender Maps to how much spender can spend
-    mapping(address => mapping(address => uint)) public allowance;
-
-    //Emit Event
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value); 
-
-    constructor() {
-        //Update Record, Deploy Token
-        balances[msg.sender] = totalSupply;
+//---------------------------------------------------------
+//                    Safe Math Library
+//---------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
     }
 
-    //Read balances for any address
-    function balanceOf(address owner) public view returns(uint) {
-        //Find address in mapping record
-        return balances[owner];
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        c = a - b;
+        require(b <= a);
     }
 
-    //
-    function transfer(address to, uint value) public returns(bool) {
-        //Check Effect Interacts Security Pattern Design Here
-        require(balanceOf(msg.sender) >= value, 'Failed: Insuffcient Balance Avaliable'); //Check
-        balances[to] += value; //Effect
-        balances[msg.sender] -= value; //Effect
-        emit Transfer(msg.sender, to, value); //Interacts
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a== 0 || c/a == b);
+    }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+//---------------------------------------------------------
+//                 QUIVER (QIVC) Token
+//---------------------------------------------------------
+contract Quiver is ERC20Interface, SafeMath {
+    string public name;
+    string public symbol;
+    uint8 public decimals; //18 decimals is strongly suggested as defualt
+
+    uint256 public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    /**
+    * Constructor Function
+    *
+    *Intialise contract with intial supply tokens to the creator of the smart contract
+    **/
+    constructor() public {
+        name = "Quiver";
+        symbol = "QUIV";
+        decimals = 18;
+        _totalSupply = 1000000000;
+
+        balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+
+    function totalSupply() public view returns (uint) {
+        return _totalSupply  - balances[address(0)];
+    }
+
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }
 
-    function transferFrom(address from, address to, uint value) public returns(bool) {
-        //Check Effect Interacts Security Pattern Design Here
-        require(balanceOf(from) >= value, 'Failed: Insuffcient Balance Avaliable'); //Check
-        //Check sender of this transaction is an approved spender for the from address
-        require(allowance[from][msg.sender] >= value, 'Failed: Allowance To Low'); //Check
-        balances[to] += value; //Effect
-        balances[from] -= value; //Effect
-        emit Transfer(from, to, value); //Interact
-        return true;  
+    function transfer(address to, uint tokens) public returns (bool success) {
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
     }
 
-    function approve(address spender, uint value) public returns(bool) {
-        //Access nested mapping
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true; 
+    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
     }
-
 }
